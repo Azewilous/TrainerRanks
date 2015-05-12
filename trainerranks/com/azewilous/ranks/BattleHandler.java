94cb1c836163b0ef3f7658c6c01a8bc79bb3fae6
Azewilous <lucarzard@gmail.com>
2015-05-11 21:47:41 -0400

    Fixed Battle Handler Error For Victors, Still Working On The Defeated In Battles. Fixed Chat Error Also

--- trainerranks/com/azewilous/ranks/BattleHandler.java
+++ trainerranks/com/azewilous/ranks/BattleHandler.java
@@ -22,37 +22,45 @@ public class BattleHandler {
 		//BattleParticipant par4 = event.battleController.participants.get(3);
 		if(par1 != null && par2 != null){
 			if(par1.getType() == ParticipantType.Player && par2.getType() == ParticipantType.Player){
+				if(!par1.getEntity().worldObj.isRemote){
 						if(par1.hasRemainingPokemon()){
 							RankHandler.recieveExp((EntityPlayerMP) par1.getEntity());
 							RankHandler.rankUp((EntityPlayerMP) par1.getEntity());
 							((EntityPlayerMP)par1.getEntity()).addChatMessage(new ChatComponentTranslation(EnumChatFormatting.YELLOW 
-									+ "Victory Against " + par2.getName().getFormattedText() 
+									+ "Victory Against " + par2.getName().getUnformattedTextForChat()
 									+ EnumChatFormatting.YELLOW + ", You Now Have " + RankHandler.getExp((EntityPlayerMP) par1.getEntity())
 									+ EnumChatFormatting.YELLOW + " PokeExp"));
-						}else{
+						}else
+							if(!par1.hasRemainingPokemon()){
 							RankHandler.looseExp((EntityPlayerMP) par1.getEntity());
 							RankHandler.rankUp((EntityPlayerMP) par1.getEntity());
 							((EntityPlayerMP)par1.getEntity()).addChatMessage(new ChatComponentTranslation(EnumChatFormatting.YELLOW 
-									+ "Defeated By " + par2.getName().getFormattedText() 
+									+ "Defeated By " + par2.getName().getUnformattedTextForChat()
 									+ EnumChatFormatting.YELLOW + ", You Now Have " + RankHandler.getExp((EntityPlayerMP) par1.getEntity())
 									+ EnumChatFormatting.YELLOW + " PokeExp"));
-						}
+						}else
+							return;
+				}
+							if(!par2.getEntity().worldObj.isRemote){
 								if(par2.hasRemainingPokemon()){
 									RankHandler.recieveExp((EntityPlayerMP) par2.getEntity());
 									RankHandler.rankUp((EntityPlayerMP) par2.getEntity());
 									((EntityPlayerMP)par2.getEntity()).addChatMessage(new ChatComponentTranslation(EnumChatFormatting.YELLOW 
-											+ "Victory Against " + par1.getName().getFormattedText() 
+											+ "Victory Against " + par1.getName().getUnformattedTextForChat()
 											+ EnumChatFormatting.YELLOW + ", You Now Have " + RankHandler.getExp((EntityPlayerMP) par2.getEntity())
 											+ EnumChatFormatting.YELLOW + " PokeExp"));
-								}else{
+								}else
+									if(!par2.hasRemainingPokemon()){
 									RankHandler.looseExp((EntityPlayerMP) par1.getEntity());
 									RankHandler.rankUp((EntityPlayerMP) par1.getEntity());
 									((EntityPlayerMP)par1.getEntity()).addChatMessage(new ChatComponentTranslation(EnumChatFormatting.YELLOW 
-											+ "Defeated By " + par2.getName().getFormattedText() 
+											+ "Defeated By " + par2.getName().getUnformattedTextForChat()
 											+ EnumChatFormatting.YELLOW + ", You Now Have " + RankHandler.getExp((EntityPlayerMP) par1.getEntity())
 											+ EnumChatFormatting.YELLOW + " PokeExp"));
 								}
-			}
+							}
+			}else
+				return;
 		}
 	}
 }
